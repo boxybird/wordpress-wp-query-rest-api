69be2e8e6e831db0f4ebc676a4d5d691254706a7
boxybird <andrew.rhyand@gmail.com>
2020-11-24 17:54:11 -0800

    bump v0.0.7

--- boxybird-wp-query-endpoint.php
+++ boxybird-wp-query-endpoint.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       WP_Query REST API Endpoint
  * Description:       Query anything you want from the WordPress database using a single REST API endpoint.
- * Version:           0.0.6
+ * Version:           0.0.7
  * Author:            Andrew Rhyand
  * Author URI:        https://andrewrhyand.com
  * License:           GPLv2 or later
